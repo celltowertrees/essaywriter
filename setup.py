f8159fe20895c310ca265442bdc6b89948ac1ac1
dataface <monson.exe@gmail.com>
2013-12-19 16:18:15 -0500

    updated installation reqs

--- setup.py
+++ setup.py
@@ -7,10 +7,10 @@ config = {
     'description': 'Twitter Essays',
     'author': 'Kelly Monson',
     'url': 'http://kmonson.com',
-    'download_url': 'http://kmonson.com',
+    'download_url': ' ',
     'author_email': 'monson.exe@gmail.com',
     'version': '0.1',
-    'install_requires': ['nose'],
+    'install_requires': ['nose', 'requests', 'BeautifulSoup4'],
     'packages': ['twitteressays'],
     'scripts': ['bin/app.py'],
     'name': 'twitteressays'
