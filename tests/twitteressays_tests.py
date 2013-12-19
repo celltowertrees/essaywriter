53b30b513ffbd5b4085010527f5cf2ad2258f284
dataface <monson.exe@gmail.com>
2013-12-19 09:55:57 -0500

    Beginning of a test

--- tests/twitteressays_tests.py
+++ tests/twitteressays_tests.py
@@ -1,6 +1,11 @@
 from nose.tools import *
 import twitteressays
 
+def test_extract():
+    list = []
+    stuff = open(test.txt, 'r')
+    assert_equal(extract(stuff, list), "My biggest issue with Duck Dynasty is that it&#39;s perpetuating our acceptance of garbage as art.")
+
 def setup():
     print "SETUP!"
     
