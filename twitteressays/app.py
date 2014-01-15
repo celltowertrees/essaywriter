31c1c14dc0513d9c64e0796d608f4545cb488216
dataface <monson.exe@gmail.com>
2014-01-15 12:27:39 -0500

    templates directory

--- twitteressays/app.py
+++ twitteressays/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
+from write import Writer
+
 
 app = Flask(__name__)
 app.debug = True
@@ -6,6 +8,20 @@ app.debug = True
 @app.route('/', methods=['GET'])
 def index():
 	return render_template('index.html')
+
+
+@app.route('/essay', methods=['POST'])
+def essay():
+	keyword = request.form['keyword']
+
+	if not keyword:
+		return redirect(url_for('index'))
+
+	essay = Writer(keyword)
+	result = essay.write()
+
+	return render_template('essay.html', result=result, keyword=keyword)
+
 
 if __name__ == '__main__':
 	app.run()
