f5c6c203e52943dd4e41af1242c60fcbb361b830
dataface <monson.exe@gmail.com>
2014-03-29 19:17:35 -0400

    Corrected whitespace.

--- twitteressays/write.py
+++ twitteressays/write.py
@@ -9,9 +9,11 @@ from bs4 import BeautifulSoup
 from pymarkovchain import MarkovChain
 from random import choice, shuffle
 
+
 # TODO:
 # Add more keyword options
 # Separate into more modules with more options
+
 
 class Writer(object):
 
@@ -19,9 +21,11 @@ class Writer(object):
         self.keyword = keyword
         self.links = self.extract_links_wikipedia()
 
-
     def extract_links_text(self, file):
-        """ Reads from a text file of source links. Returns the links in a list. """
+        """
+        Reads from a text file of source links.
+        Returns the links in a list.
+        """
         links = []
 
         with open(file, "r") as file_obj:
@@ -29,15 +33,13 @@ class Writer(object):
 
             try:
                 links = lines.split('\n')
-            except AttributeError:
-                links = lines
+                for line in lines:
+                    links.append(line)
 
-            for line in lines:
-                links.append(line)
+            except AttributeError:
+                print "No."
 
         return links
-
-
 
     def extract_links_wikipedia(self):
         """ Find source links in a Wikipedia article. """
@@ -64,7 +66,6 @@ class Writer(object):
         else:
             raise NameError('TRY A LITTLE BIT HARDER')
 
-    
     def generateModel(self, essay):
         """ Generate a Markov chain based on retrieved strings. """
 
@@ -82,7 +83,6 @@ class Writer(object):
 
         return result
 
-
     def analyze(self, text):
         """ Takes a collection of adjectives using NLTK tokenizer. """
 
@@ -98,14 +98,17 @@ class Writer(object):
 
         return adjectives
 
-
     def read_articles(self, link_list):
-        """ Request articles, inspect markup for any important strings, and return them, cleaned """
-        
+        """
+        Request articles,
+        inspect markup for any important strings,
+        and return them, cleaned
+        """
         info = ''
 
         def get_tag(tag):
-            # Whatever tag is passed, the text inside of it is cleaned up and inserted into a bigger string
+            # Whatever tag is passed, the text inside of it is cleaned up a
+            # nd inserted into a bigger string
             if soup.find(tag):
                 tag_text = ''
                 print "Reading..."
@@ -118,7 +121,7 @@ class Writer(object):
             else:
                 pass
 
-        for url in link_list: 
+        for url in link_list:
             # Each URL gets its own string of returned text
             full_article = ''
 
@@ -126,10 +129,10 @@ class Writer(object):
                 r = requests.get(url, timeout=5)
                 print "Opening article"
                 soup = BeautifulSoup(r.text)
-                
+
                 if get_tag('p'):
                     full_article += get_tag('p')
-                else: 
+                else:
                     pass
 
             except requests.exceptions.RequestException:
@@ -139,14 +142,11 @@ class Writer(object):
 
         return info
 
-
     # Generates essay
-
     def write(self):
         """ Generator """
         articles = self.read_articles(self.links)
         return self.analyze(articles)
-
 
 
 #------------OLD SHIT----------------
@@ -155,7 +155,6 @@ def extract_tweets(tag):
     r = requests.get('https://twitter.com/search?q=' + tag + '&src=typd')
     soup = BeautifulSoup(r.text)
     text = ''
-    
     for t in soup.find_all('p', class_="tweet-text"):
         for a in t.find_all('a'):
             a.decompose()
@@ -167,4 +166,3 @@ def extract_tweets(tag):
 def formatString(string):
     text = string.replace('"', '')
     return text
-
